6734f2c19cbfb08fc5599c8351c2b1e0322059c0
AnnaLaura08 <annalaura.sales@icloud.com>
2026-05-07 09:35:55 -0300

    tualizacao

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,21 +10,19 @@ function Home() {
 
     return (
         <div className={styles.homeContainer}>
-            <div className={styles.banner}> 
-            <h1>Bem-vindo à Lauhy Joias!</h1>
-            <p>Explore nossa coleção de joias exclusivas e encontre a peça perfeita para você.</p>
+            <div className={styles.banner}>
+                <h1>Bem-vindo à Lauhy Joias!</h1>
+                <p>
+                    Explore nossa coleção de joias exclusivas e encontre a peça perfeita para você.
+                </p>
             </div>
-            
-            <div className={styles.cardsContainer}>
-                {cards.map(card => (
-                    <Card 
-                        key={card.id} 
-                        title={card.title} 
-                        description={card.description} 
-                    />
+
+            <div className={styles.cardsGrid}>
+                {cards.map((card) => (
+                    <Card key={card.id} title={card.title} description={card.description} />
                 ))}
             </div>
-        </div> 
+        </div>
     );
 }
 
